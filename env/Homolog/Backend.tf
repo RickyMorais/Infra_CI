a72e96287e87fac1f97d5ed92839e2783790e668
Henrique de Souza Morais <henriquemoraiswork@gmail.com>
2025-06-24 01:05:14 -0300

    Configurando terraform para o meu bucket do s3

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-alura"
-    key    = "Prod/terraform.tfstate"
+    bucket = "terraform-state-ricky"
+    key    = "Homolog/terraform.tfstate"
     region = "us-west-2"
   }
 }
