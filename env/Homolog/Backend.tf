5ecfc52bc0d9978f46c7db7131b340e8bf763dbb
Henrique de Souza Morais <henriquemoraiswork@gmail.com>
2025-06-24 20:17:33 -0300

    correção

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-ricky"
-    key    = "Homolog/terraform.tfstate"
+    key    = "Prod/terraform.tfstate"
     region = "us-west-2"
   }
 }
